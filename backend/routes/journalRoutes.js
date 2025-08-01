9e3436a6732cc2beca3dbf3a59f746a5555c2d55
Manvitha-Appana <appanamanvitha14@gmail.com>
2025-08-01 20:01:15 +0530

    Update journalRoutes.js

--- backend/routes/journalRoutes.js
+++ backend/routes/journalRoutes.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('./authMiddleware');
+
 router.delete("/:id", auth, async (req, res) => {
     await Journal.deleteOne({ _id: req.params.id, user: req.user.userId });
     res.json({ message: "Deleted" });
